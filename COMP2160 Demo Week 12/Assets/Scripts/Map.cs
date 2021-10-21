d28f3ee9a40ff19b1a9fb738b6c9f7a1e7628410
Malcolm Ryan <malcolm.ryan@gmail.com>
2021-10-21 17:38:46 +1100

    Added map with simple path planner

---
+++ COMP2160 Demo Week 12/Assets/Scripts/Map.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    [SerializeField]
+    private TileMap tileMap;
+
+    public List<Vector3Int> Path(Vector3Int src, Vector3Int dest)
+    {
+        // simple path planner with no obstacle avoidance
+        
+        if (IsOccupied(dest))
+        {
+            return null;
+        }
+
+        List<Vector3Int> path = new List<Vector3Int>();
+        path.Add(src);
+
+        Vector3Int dist = dest - src;
+        Vector3Int dir;
+        dir.x = Math.Sign(dist.x);
+        dir.y = Math.Sign(dist.y);
+        dist.x = Math.Abs(dist.x);
+        dist.y = Math.Abs(dist.y);
+
+        Vector3Int p = src;
+        while (dist.x > 0 || dist.y > 0)
+        {
+            if (dist.x > dist.y)
+            {
+                // take one step in the x direction
+                p.x += dir.x;
+                dist.x--;
+            }
+            else if (dist.x < dist.y)
+            {
+                // take one step in the y direction
+                p.y += dir.sy;
+                dist.y--;
+            }
+            else {
+                // take one step along the diagonal
+                p.x += dir.x;
+                p.y += dir.y;
+                dist.x--;
+                dist.y--;
+            }
+
+            path.Add(p);
+        }
+        return path;
+    }
+
+
+    public List<Vector3Int> Path(Vector3 src, Vector3 dest)
+    {        
+        return Path(Vector3Int.FloorToInt(src),
+            Vector3Int.FloorToInt(dest));
+    }
+
+    public bool IsOccupied(Vector3Int p)
+    {
+        return tileMap.GetColliderType(p) != ColliderType.None;
+    }
+
+    public bool IsOccupied(Vector3 p)
+    {
+        return IsOccupied(Vector3Int.FloorToInt(p));
+    }
+
+
+}
